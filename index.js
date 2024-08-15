da31b20af8f385875aea2634ad6cb28156b54e9b
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-08-15 11:32:17 +0600

    mongodb te 40 data setup

--- index.js
+++ index.js
@@ -25,14 +25,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const database = client.db("myProducst");
+
+    // Create a new collection
+    const collection = database.collection("products");
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
